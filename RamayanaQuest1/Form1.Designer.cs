288e775cdd6613898cf4d8ad01188ab0c18c7f07
Кирилл Желиховский <KirillZhelihovskij@rambler.ru>
2023-10-15 19:23:22 +0300

    Добавьте файлы проекта.

---
+++ RamayanaQuest1/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace RamayanaQuest1
+{
+    partial class RamayanaQuest1Form
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.questionLabel = new System.Windows.Forms.Label();
+            this.optionAButton = new System.Windows.Forms.Button();
+            this.optionBButton = new System.Windows.Forms.Button();
+            this.optionCButton = new System.Windows.Forms.Button();
+            this.optionDButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // questionLabel
+            // 
+            this.questionLabel.AutoSize = true;
+            this.questionLabel.Location = new System.Drawing.Point(265, 55);
+            this.questionLabel.Name = "questionLabel";
+            this.questionLabel.Size = new System.Drawing.Size(0, 13);
+            this.questionLabel.TabIndex = 0;
+            // 
+            // optionAButton
+            // 
+            this.optionAButton.Location = new System.Drawing.Point(184, 151);
+            this.optionAButton.Name = "optionAButton";
+            this.optionAButton.Size = new System.Drawing.Size(75, 23);
+            this.optionAButton.TabIndex = 1;
+            this.optionAButton.UseVisualStyleBackColor = true;
+            // 
+            // optionBButton
+            // 
+            this.optionBButton.Location = new System.Drawing.Point(184, 200);
+            this.optionBButton.Name = "optionBButton";
+            this.optionBButton.Size = new System.Drawing.Size(75, 23);
+            this.optionBButton.TabIndex = 2;
+            this.optionBButton.UseVisualStyleBackColor = true;
+            // 
+            // optionCButton
+            // 
+            this.optionCButton.Location = new System.Drawing.Point(184, 250);
+            this.optionCButton.Name = "optionCButton";
+            this.optionCButton.Size = new System.Drawing.Size(75, 23);
+            this.optionCButton.TabIndex = 3;
+            this.optionCButton.UseVisualStyleBackColor = true;
+            // 
+            // optionDButton
+            // 
+            this.optionDButton.Location = new System.Drawing.Point(184, 297);
+            this.optionDButton.Name = "optionDButton";
+            this.optionDButton.Size = new System.Drawing.Size(75, 23);
+            this.optionDButton.TabIndex = 4;
+            this.optionDButton.UseVisualStyleBackColor = true;
+            // 
+            // RamayanaQuest1Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.optionDButton);
+            this.Controls.Add(this.optionCButton);
+            this.Controls.Add(this.optionBButton);
+            this.Controls.Add(this.optionAButton);
+            this.Controls.Add(this.questionLabel);
+            this.Name = "RamayanaQuest1Form";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label questionLabel;
+        private System.Windows.Forms.Button optionAButton;
+        private System.Windows.Forms.Button optionBButton;
+        private System.Windows.Forms.Button optionCButton;
+        private System.Windows.Forms.Button optionDButton;
+    }
+}
+
---
+++ RamayanaQuest1/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace RamayanaQuest1
+{
+    public partial class RamayanaQuest1Form : Form
+    {
+        private int currentQuestionIndex;
+
+        private Question[] questions;
+
+        public RamayanaQuest1Form()
+        {
+            InitializeComponent();
+            InitializeQuestions();
+            ShowQuestion(0);
+        }
+
+        private void InitializeQuestions()
+        {
+            // Инициализация вопросов
+            questions = new Question[]
+            {
+                new Question("Кто является главным героем Рамаяны?",
+                    new string[] {
+                        "Арджуна",
+                        "Рама",
+                        "Кришна",
+                        "Индра"
+                    },
+                    1),
+                new Question("Какое оружие использовал Рама для уничтожения Раваны?",
+                    new string[] {
+                        "Лук",
+                        "Меч",
+                        "Копье",
+                        "Топор"
+                    },
+                    0),
+                new Question("Кто помог Раме в его походе на Ланку?",
+                    new string[] {
+                        "Хануман",
+                        "Брахма",
+                        "Гаруда",
+                        "Ханумати"
+                    },
+                    0),
+                new Question("Кто является воплощением богини Лакшми и женой Рамы?",
+                    new string[] {
+                        "Сита",
+                        "Савитри",
+                        "Драупади",
+                        "Радха"
+                    },
+                    0),
+                new Question("Какой символ является признаком Раваны?",
+                    new string[] {
+                        "Десять голов",
+                        "Девять голов",
+                        "Восемь голов",
+                        "Семь голов"
+                    },
+                    2)
+            };
+        }
+
+        private void ShowQuestion(int questionIndex)
+        {
+            // Отображение текущего вопроса
+            currentQuestionIndex = questionIndex;
+
+            Question currentQuestion = questions[currentQuestionIndex];
+
+            questionLabel.Text = currentQuestion.Text;
+
+            optionAButton.Text = currentQuestion.Options[0];
+            optionBButton.Text = currentQuestion.Options[1];
+            optionCButton.Text = currentQuestion.Options[2];
+            optionCButton.Text = currentQuestion.Options[3];
+        }
+
+        private void CheckAnswer(int answerIndex)
+        {
+            // Проверка выбранного ответа
+            Question currentQuestion = questions[currentQuestionIndex];
+
+            if (answerIndex == currentQuestion.CorrectOption)
+            {
+                MessageBox.Show("Правильный ответ!");
+            }
+            else
+            {
+                MessageBox.Show("Неправильный ответ!");
+            }
+
+            // Переход к следующему вопросу
+            if (currentQuestionIndex < questions.Length - 1)
+            {
+                ShowQuestion(currentQuestionIndex + 1);
+            }
+            else
+            {
+                MessageBox.Show("Квест завершен!");
+                Application.Exit();
+            }
+        }
+
+        private void optionAButton_Click(object sender, EventArgs e)
+        {
+            CheckAnswer(0);
+        }
+
+        private void optionBButton_Click(object sender, EventArgs e)
+        {
+            CheckAnswer(1);
+        }
+
+        private void optionCButton_Click(object sender, EventArgs e)
+        {
+            CheckAnswer(2);
+        }
+
+        private void optionDButton_Click(object sender, EventArgs e)
+        {
+            CheckAnswer(3);
+        }
+    }
+
+    public class Question
+    {
+        public string Text { get; private set; }
+        public string[] Options { get; private set; }
+        public int CorrectOption { get; private set; }
+
+        public Question(string text, string[] options, int correctOption)
+        {
+            Text = text;
+            Options = options;
+            CorrectOption = correctOption;
+        }
+    }
+}
